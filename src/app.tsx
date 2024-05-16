646a9007913156e17a22d9b24bb961ccd276d692
Graff Alexandre <alexgraff67@gmail.com>
2024-05-16 15:11:54 +0200

    0.0.2 Affichage de la liste des pokémons

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,36 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import { POKEMONS } from './models/mock-pokemon'
   
 const App: FunctionComponent = () => {
-    const [pokemons] = useState<Pokemon[]>(POKEMONS);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+      setPokemons(POKEMONS);
+    }, []);
     
  return (
   <div>
-    <h1>Pokédex</h1>
+    <h1 className='center'>Pokédex</h1>
+    <div className='container'>
+      <div className='row'>
+        {pokemons.map(({ id, name, picture, created }) => (
+          <div className='col s6 m4' key={id}>
+            <div className='card horizontal'>
+              <div className='card-image'>
+                <img src={picture} alt={name}/>
+              </div>
+              <div className='card-stacked'>
+                <div className='card-content'>
+                  <p>{name}</p>
+                  <p><small>{created.toString()}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+      ))}
+      </div>
+    </div>
     <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
   </div>
  )
